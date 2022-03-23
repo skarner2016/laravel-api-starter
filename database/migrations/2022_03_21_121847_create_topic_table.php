baedf9cf74232ca3c4fb71eedaf82079b8327c3b
skarner2016 <skarner2016@gmail.com>
2022-03-23 19:10:25 +0800

    update: topic_table

--- database/migrations/2022_03_21_121847_create_topic_table.php
+++ database/migrations/2022_03_21_121847_create_topic_table.php
@@ -17,7 +17,11 @@ return new class extends Migration
     {
         Schema::create($this->tableName, function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id')->comment('用户ID');
             $table->text('content')->comment('主题内容');
+            $table->unsignedInteger('hot')->default(0)->comment('热度');
+            $table->unsignedTinyInteger('is_digest')->default(0)->comment('是否精华:1是0否');
+            $table->unsignedTinyInteger('audit_status')->default(0)->comment('审核状态:0待审核,10通过,20拒绝');
             $table->timestamps();
         });
 
